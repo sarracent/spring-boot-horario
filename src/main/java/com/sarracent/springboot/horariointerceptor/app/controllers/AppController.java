69bcf4440b1d5de9ea9dc75475109bca9c337360
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-10 23:27:28 -0300

    first commit

---
+++ src/main/java/com/sarracent/springboot/horariointerceptor/app/controllers/AppController.java
@@ -0,0 +1,14 @@
+package com.sarracent.springboot.horariointerceptor.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("titulo", "Bienvenido al horario de atencion a clientes");
+        return "index";
+    }
+}
